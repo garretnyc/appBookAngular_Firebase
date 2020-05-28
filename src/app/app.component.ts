a2460c17a9acfbbd08837b4016827d644223b315
garretuk <mclawicanadauk@hotmail.com>
2020-05-28 17:34:13 +0200

    general settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from "firebase/app";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projectFirebase';
+  constructor(){
+            // TODO: Replace the following with your app's Firebase project configuration
+            const firebaseConfig = {
+              apiKey: "AIzaSyAa162uzMS9nE0dqYT796oyB9v4tESMdLk",
+              authDomain: "fir-project-3d9b4.firebaseapp.com",
+              databaseURL: "https://fir-project-3d9b4.firebaseio.com",
+              projectId: "fir-project-3d9b4",
+              storageBucket: "fir-project-3d9b4.appspot.com",
+              messagingSenderId: "789005580234",
+              appId: "1:789005580234:web:7655bc542c384ed3da8133",
+              measurementId: "G-6W86YXLJ00"
+            };
+            
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+            
+              }
+  
 }
